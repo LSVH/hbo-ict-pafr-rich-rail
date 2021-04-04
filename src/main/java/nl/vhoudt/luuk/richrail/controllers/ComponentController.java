34ba4f3ef0b7e7175c46f8774438ea83f9121105
Luuk van Houdt <luuk@vhoudt.nl>
2021-04-04 23:00:40 +0200

    Added (de)attach functionallity

--- src/main/java/nl/vhoudt/luuk/richrail/controllers/ComponentController.java
+++ src/main/java/nl/vhoudt/luuk/richrail/controllers/ComponentController.java
@@ -6,6 +6,10 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.json.JsonParser;
 import org.springframework.boot.json.JsonParserFactory;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PatchMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
@@ -38,5 +42,32 @@ public class ComponentController {
         Component res = service.save(new Component((Integer) json.get("spot"), train, type));
 
         return ResponseEntity.ok(res);
+    }
+
+    @DeleteMapping(path = "/component/{componentId}")
+    public ResponseEntity<Object> updateTrain(@PathVariable Integer componentId) {
+        Component component = service.findById(componentId);
+        
+        service.delete(component);
+
+        return ResponseEntity.ok().build();
+    }
+
+    @PatchMapping(path = "/component/{componentId}/train/{trainId}")
+    public ResponseEntity<Object> updateTrain(@PathVariable Integer componentId, @PathVariable Integer trainId) {
+        Component component = service.findById(componentId);
+        
+        Train train = trainService.findById(trainId);
+        component.setTrain(train);
+        service.save(component);
+
+        return ResponseEntity.ok(train);
+    }
+
+    @GetMapping(path = "/component/{componentId}")
+    public ResponseEntity<Object> read(@PathVariable Integer componentId) {
+        Component component = service.findById(componentId);
+
+        return ResponseEntity.ok(component);
     }
 }
